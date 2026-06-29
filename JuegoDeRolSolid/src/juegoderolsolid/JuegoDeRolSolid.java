cc1a91ac0e9d1097b64fa173a8c99deaccd13299
Josue <jotue.777@gmail.com>
2026-06-28 22:28:46 -0500

    main vacio inicial

---
+++ JuegoDeRolSolid/src/juegoderolsolid/JuegoDeRolSolid.java
@@ -0,0 +1,12 @@
+package juegoderolsolid;
+
+/**
+ *
+ * @author jotue
+ */
+public class JuegoDeRolSolid {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
